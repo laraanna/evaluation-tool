931da53391843a167733101f8fbfb81c5839ee28
laraanna <lara@laraannawagner.com>
2017-12-12 15:56:29 +0100

    Add Navbar

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,18 +7,19 @@ export const darkRed      = '#C1272D'
 export const white        = '#ffffff'
 export const black        = '#000000'
 export const darkGrey     = '#757575'
-export const grey         = '#DEDEDE'
+export const grey         = '#E5E5E5'
 export const grey50       = 'rgba(222, 222, 222, 0.5)'
 export const grey30       = 'rgba(222, 222, 222, 0.7)'
 
+
 // Palette
 export const palette = {
-  primary1Color: red,
+  primary1Color: grey,
   primary2Color: green,
   primary3Color: green,
   accent1Color: green,
   textColor: black,
-  alternateTextColor: white,
+  alternateTextColor: darkRed,
   canvasColor: white,
   borderColor: grey,
   disabledColor: grey30
