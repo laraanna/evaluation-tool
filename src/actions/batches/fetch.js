13a0a8ac3babce2eaf06d81e3e71fd65e6b33cef
laraanna <lara@laraannawagner.com>
2017-12-12 16:43:08 +0100

    Add Title component

--- src/actions/batches/fetch.js
+++ src/actions/batches/fetch.js
@@ -1,1 +1,24 @@
 import ApiClient from '../../api/client'
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  LOAD_ERROR,
+  LOAD_SUCCESS
+} from '../loading'
+
+
+export const FETCHED_BATCHES = 'FETCHED_BATCHES'
+
+const api = new ApiClient()
+
+export default () => {
+  return dispatch => {
+    // dispatch(loading(true)) // ???
+
+    api.get(???)
+      .then(res => dispatch({ type: FETCHED_BATCHES, payload: res.body }))
+      //.catch(err => dispatch(loadError(err))) ???
+
+    // dispatch(loading(false)) // ???
+  }
+}
