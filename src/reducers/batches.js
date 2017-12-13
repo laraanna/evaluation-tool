62c981cfdcbd23b934e355d70b16010a93063807
laraanna <lara@laraannawagner.com>
2017-12-13 11:27:31 +0100

    Add create batch action

--- src/reducers/batches.js
+++ src/reducers/batches.js
@@ -1,10 +1,19 @@
 import { FETCHED_BATCHES } from '../actions/batches/fetch'
+import {
+  BATCH_CREATED,
+
+} from '../actions/batches/subscribe'
 
 
 export default (state = [], {type, payload} = {}) => {
   switch(type) {
     case FETCHED_BATCHES :
       return [ ...payload ]
+
+    case BATCH_CREATED :
+      const newBatch = { ...payload }
+      return [newBatch].concat(state)
+
 
 
 
