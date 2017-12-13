57598ce9e06aa771c7fda874460fe140aee654a6
laraanna <lara@laraannawagner.com>
2017-12-13 10:14:34 +0100

    Add fetch Batches to Container

---
+++ src/containers/Batch.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Title from '../components/Title'
+
+
+
+
+class Batch extends PureComponent {
+
+
+
+  render() {
+
+    return(
+      <div className="Batch">
+
+      </div>
+    )
+  }
+}
+
+
+
+export default Batch
